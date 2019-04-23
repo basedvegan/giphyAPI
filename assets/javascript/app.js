70cb73f71cb47a4410f942d0479c09265eb7d9fa
basedvegan <pojomanda@gmail.com>
2019-04-23 13:54:08 -0700

    ajax calling still gifs, not sure why &limit=10 isn't working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,76 @@
 //create an array of strings, each one related to a topic that interests you. Save it to a variable called topics. testing
-var topics = ["Spirited Away", "Hunter x Hunter", "Puella Magi Madoka Magica", "Full Metal Alchemist", "Cowboy Bebop", "Samurai Champloo", "Food Wars", "Guren Lagaan"];
+var topics = ["Spirited Away", "Hunter x Hunter", "Puella Magi Madoka Magica", "Full Metal Alchemist", "Cowboy Bebop", "Samurai Champloo", "Naruto", "Guren Lagaan"];
 
 
 // Your app should take the topics in this array and create buttons in your HTML.
 // Try using a loop that appends a button for each string in the array.
-function createButtons() {
-    for (var i = 0; i < topics.length; i++) {
-        var button = $('<button>' + topics[i] + '</button>');
-        button.addClass("anime-title");
-        button.attr("data-anime", topics[i]);
+// function createButtons() {
+for (var i = 0; i < topics.length; i++) {
+    var button = $('<button>' + topics[i] + '</button>');
+    // button.addClass("anime-title");
+    button.attr("data-anime", topics[i]);
+    //append buttons to html div
+    $("#button-box").append(button);
 
-        $("#button-box").append(button);
-
+    $('button').on('click', function () {
         // check to see if buttons are actually being clicked
-        $('button').on('click', function (e) {
-            var anime = $(this).attr("data-anime");
-            //check if data attr was passed
-            //console.log(anime);
-            //add query url and check if linked to "anime"
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            anime + "&api_key=dc6zaTOxFJmzC&limit=10";
-           // console.log(queryURL);
+        //console.log("clicked");
 
-           //ajax call
-           $.ajax({
-               url: queryURL,
-               method: "GET"
-           })
-           //function for what to do with response (data array of 10 gifs)
-           .then(function(response) {
-               //check for retrieved data 
-               console.log(response);
-           })
+        //event.preventDefault();
+        var anime = $(this).attr("data-anime");
+        //check if data attr was passed
+        //console.log(anime);
+        //add query url and check if linked to "anime"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            anime + "&api_key=nEJ4U2h4TUNGRM7XscV2yEFq62tOorZ4&limit=10";
+        //console.log(queryURL);
 
+        // ajax call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
         })
-    }
-}
+            //function for what to do with response (data array of 10 gifs) from ajax call
+            .then(function (response) {
+                //console.log(queryURL);
+                //check for retrieved data 
+                //console.log(response);
+                //store data in results variable
+                var results = response.data;
+                console.log(results);
 
-createButtons();
+                //loop though each result item
+                for (var i = 0; i < results.length; i++) {
+                    //create and store div tag
+                    var animeDiv = $("<div>");
+
+                    //creating p tag for result item's rating
+                    var p = $("<p>").text("rating:" + results[i].rating);
+
+                    //creating and storing img tag
+                    var animeGIF = $("<img>");
+
+                    //setting the src attr of the img to the still img property to get back the still images
+                    animeGIF.attr("src", results[i].images.fixed_height_still.url);
+
+                    //appending paragraph and image tag to the animeDiv
+
+                    animeDiv.append(animeGIF);
+                    animeDiv.append(p);
+
+                    //prepending the animeDiv to the HTML page in the appropriate div
+                    $("#gif-rating-box").prepend(animeDiv);
+
+
+
+                }
+            });
+
+    });
+}
+// }
+
+// createButtons();
 
 // checking out my api key
 // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=nEJ4U2h4TUNGRM7XscV2yEFq62tOorZ4&limit=5");
@@ -54,6 +87,3 @@ createButtons();
 // Only once you get images displaying with button presses should you move on to the next step.
 
 
-// Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-// Deploy your assignment to Github Pages.
-// Rejoice! You just made something really cool.
