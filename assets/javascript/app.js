21967afe71bd748d514e610737b46753db9b657e
basedvegan <pojomanda@gmail.com>
2019-04-22 17:19:08 -0700

    added jquery buttons and testing api key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,29 @@
 var topics = ["Spirited Away", "Hunter x Hunter", "Puella Magi Madoka Magica", "Full Metal Alchemist", "Cowboy Bebop", "Samurai Champloo", "Food Wars", "Guren Lagaan"];
 
 
-
-
-
 // Your app should take the topics in this array and create buttons in your HTML.
 // Try using a loop that appends a button for each string in the array.
+for (var i = 0; i < topics.length; i++) {
+    var button = $('<button>' + topics[i] + '</button>');
 
+    $("#button-box").append(button);
+
+    // check to see if buttons are actually being clicked
+    $('button').on('click', function () {
+        console.log('button clicked!');
+    })
+
+
+
+
+}
+
+// checking out my api key
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=nEJ4U2h4TUNGRM7XscV2yEFq62tOorZ4&limit=5");
+xhr.done(function (data) { console.log("success got data", data); });
 
 // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
 // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
 // Under every gif, display its rating (PG, G, so on).
